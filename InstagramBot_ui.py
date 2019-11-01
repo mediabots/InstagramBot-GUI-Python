b2c4b8048e5a71a6ea70b1c2ae4ae8ea7f475278
MediaBOTS <46617144+mediabots@users.noreply.github.com>
2019-11-01 22:20:37 +0530

    user interface(UI) file in Python
    Generated from InstagramBot.ui file under ui folder.
    Generated through PySide/PyQt4
    
    -  for App (ver - 1.0.3)

--- InstagramBot_ui.py
+++ InstagramBot_ui.py
@@ -7,7 +7,10 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PySide import QtCore, QtGui
+try:
+	from PySide import QtCore, QtGui
+except:
+	from PyQt4 import QtCore, QtGui
 
 class Ui_InstagramBot(object):
     def setupUi(self, InstagramBot):
